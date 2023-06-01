856cc26a264f198abe3e8e5206b73c3ccf85c3a8
MiNeris0 <seikolor@gmail.com>
2023-06-01 11:23:24 +0300

    First commit.

---
+++ HW_4_1/HW_4_1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HW_4_1
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
