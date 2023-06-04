192977cb09e01c11a213504db063c3d1e051fa31
MiNeris0 <seikolor@gmail.com>
2023-06-04 21:41:06 +0300

    New Updates.

---
+++ HW_4_1/HW_4_1/Dtos/Responses/ErrorResponse.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_4_1.Dtos.Responses
+{
+    public class ErrorResponse
+    {
+        [JsonProperty(PropertyName = "error")]
+        public string ErrorMessage { get; set; }
+    }
+}
